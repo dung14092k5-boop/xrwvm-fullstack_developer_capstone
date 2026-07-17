fa6ec18095896c0199bbd4347b1d3b4f360c600a
Trung Dung <dung14092k5@gmail.com>
2026-07-18 04:42:42 +0700

    Task 3: Update About Us page

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,29 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import LoginPanel from "./components/Login/Login";
+import Dealers from "./components/Dealers/Dealers";
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
     <Routes>
+      {/* Default */}
+      <Route path="/" element={<Navigate to="/dealers" replace />} />
+
+      {/* Authentication */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/* Dealer Pages */}
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
+
+      {/* Unknown Route */}
+      <Route path="*" element={<Navigate to="/dealers" replace />} />
     </Routes>
   );
 }
+
 export default App;
