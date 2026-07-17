9cac05287b332d25369ac94d828a7808ce543a6c
Trung Dung <dung14092k5@gmail.com>
2026-07-18 05:12:37 +0700

    Task 3: Complete About Us static page structure

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,75 +1,15 @@
-import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from "react-router-dom";
-
-
-import Header from "./components/Header/Header";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-
-
-
 function App() {
-
-
-    return (
-
-        <Router>
-
-
-            <Header />
-
-
-            <Routes>
-
-
-                <Route 
-                    path="/"
-                    element={<Login />}
-                />
-
-
-                <Route 
-                    path="/login"
-                    element={<Login />}
-                />
-
-
-
-                <Route
-                    path="/register"
-                    element={<Register />}
-                />
-
-
-
-                <Route
-                    path="/about"
-                    element={<About />}
-                />
-
-
-
-                <Route
-                    path="/contact"
-                    element={<Contact />}
-                />
-
-
-            </Routes>
-
-
-        </Router>
-
-    );
-
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
-
 
 export default App;
